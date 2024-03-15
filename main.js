71c26ffd92e55b5f109c716e470c52cbb76b5e43
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-03-15 10:40:19 +0000

    alteracoes

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ console.log(botoes);
 for (let i=0; i < botoes.length; i++) {
     console.log(i);
 }
+for (let i=0; i <botoes.length; i++){
+    botoes[i].onclick = function() {
+        botoes[i].classList.add("ativo";)
+    }
+}
