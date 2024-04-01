0ed2b28594155bd2611a2b829252160a4f45157f
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-04-01 01:14:51 +0000

    alteracoes

--- main.js
+++ main.js
@@ -15,8 +15,14 @@ textos[i].classList.add("ativo");
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-11-17T00:00:00");
+const tempoObjetivo2 = new Date("2024-11-27T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-15T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo(tempos[0]);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
 
 function calculaTempo(tempoObjetivo){
      let tempoAtual = new Date();
