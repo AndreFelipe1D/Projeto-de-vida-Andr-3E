b05c2cbb2735bc7c4fb2069cc435636f74070296
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-04-01 00:00:09 +0000

    alteracoes

--- main.js
+++ main.js
@@ -17,4 +17,4 @@ const contadores = document.querySelectorAll(".contador");
 
 const tempoObjetivo1 = new Date("2024-10-05");
 
-contadores[0].textContent = "tempoObjetivo1";
+contadores[0].textContent = tempoObjetivo1;
