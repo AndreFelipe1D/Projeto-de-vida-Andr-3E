3a21281c2e6167afac9777363d31cdfe8a8b9aa1
AndreFelipe1D <114994513+AndreFelipe1D@users.noreply.github.com>
2024-04-01 00:53:38 +0000

    alteracoes

--- main.js
+++ main.js
@@ -25,6 +25,9 @@ function calculaTempo(tempoObjetivo){
      let minutos = Math.floor(segundos/60);
      let horas = Math.floor(minutos/60);
      let dias = Math.floor(horas/24);
+    
+    segundos %= 60;
+
      return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }
 
